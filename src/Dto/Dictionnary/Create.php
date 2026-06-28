8d96f814c56c38302ba44bc2fb1bc217306ad609
TonNom <arnaud.gautheyrou@gmail.com>
2026-06-29 01:42:55 +0200

    feature/dictionnary

---
+++ src/Dto/Dictionnary/Create.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Dto\Dictionnary;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Create
+{
+    #[Assert\NotBlank]
+    #[Assert\Type('integer')]
+    public ?int $language_id = null;
+
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 255)]
+    public ?string $word = null;
+
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 255)]
+    public ?string $translation = null;
+}
