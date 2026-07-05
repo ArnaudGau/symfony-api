4b5caff4c9f2e385c65f952f3a1c265c5efd04d9
TonNom <arnaud.gautheyrou@gmail.com>
2026-07-06 00:28:30 +0200

    correction pint

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -3,23 +3,23 @@
 namespace App\DataFixtures;
 
 use App\Entity\Category;
+use App\Repository\CategoryRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use App\Repository\CategoryRepository;
 
 class CategoryFixtures extends Fixture
 {
     public function __construct(
-        private CategoryRepository $categoryRepository
-    ) {}
+        private CategoryRepository $categoryRepository,
+    ) {
+    }
 
     public function load(ObjectManager $manager): void
     {
         $array = ['Alphabet', 'Food', 'Welcome'];
 
         foreach ($array as $key => $value) {
-            $existingCategory = $this->categoryRepository->findOneBy(['name'
-            => $value]);
+            $existingCategory = $this->categoryRepository->findOneBy(['name' => $value]);
             if (!$existingCategory) {
                 $category = new Category();
 
